8a6a2a0d96d4835f349dff4d9b9eba6ea4d86a01
unknown <bayu512kusuma@gmail.com>
2023-02-09 12:23:13 +0700

    add sale report

---
+++ elvo_sale_report/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'SO Report',
+    'version': '1.0',
+    'depends': ['sale_management'],
+    'summary': "Elvo Report",
+    'description': """
+        Elvo Report
+    """,
+    'data': [
+        'report/resi_el.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
